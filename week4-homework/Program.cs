2c56a90761122f5511b7aea28d757459a4f8c34e
tsikhish <tsikhishvilimariam@gmail.com>
2023-12-12 22:33:25 +0400

    week4 homework

---
+++ week4-homework/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace week4_homework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mariam Tsikhishvili");
+        }
+    }
+}
